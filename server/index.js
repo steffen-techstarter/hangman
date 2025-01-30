c3497bd734504f7efbbb0933daf1f33c890594bc
steffen-techstarter <steffen.keilbach@tn.techstarter.de>
2025-01-30 11:49:22 +0100

    server with routes

--- server/index.js
+++ server/index.js
@@ -1,21 +1,15 @@
-const express = require('express') 
-const app = express()
-const host = "localhost" // Server erreichbar unter 'localhost/8080'
-const port = 8080
-
-// app.get('/', (req, res) => { 
-//     res.send('Request received')
-// })
-
-// app.get('/jannik', (req, res) => { 
-//     res.send('Request received sölkjsöl')
-// })
+const express = require('express');
+const app = express();
+const host = 'localhost'; // Server erreichbar unter 'localhost/8080'
+const port = 8080;
 
 app.use('/', express.static('../gruppe'));
-
+app.use('/bahram', express.static('../z_bahram'));
+app.use('/jannik', express.static('../z_jannik'));
+app.use('/sascha', express.static('../z_sascha'));
+app.use('/serhii', express.static('../z_serhii'));
+app.use('/steffen', express.static('../z_steffen'));
 
 app.listen(port, host, () => {
-    console.log(`Server ready at http://${host}:${port}`)
-})
-
-
+	console.log(`Server ready at http://${host}:${port}`);
+});
